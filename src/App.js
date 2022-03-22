dd91341f946e53b470ae0dbafc91c0a892095b51
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2022-03-22 23:45:41 +0700

    Add data and playlist display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./single-sample";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen bg-gray-800">
+      <div className="rounded-md shadow-lg bg-white max-w-sm">
+        <header className="px-5 pt-5">
+          <img
+            className="rounded-md"
+            src={data.album.images[0].url}
+            alt="Album"
+            height="200"
+            width="200"
+          />
+        </header>
+        <div className="p-5">
+          <h5 className="text-gray-900 text-xl font-medium mb-2">
+            {data.name}
+          </h5>
+          <p className="text-gray-700 text-base mb-4">{data.artists[0].name}</p>
+          <button className="inline-block w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg">
+            Select
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
