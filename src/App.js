ec1368b686e3edd6a20099c9a1291ea7788dcaec
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2022-03-27 14:33:22 +0700

    Refactoring playlist view and add more data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import Song from "./components/Song";
 import data from "./single-sample";
 
+console.log(data);
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-800">
-      <Song
-        title={data.name}
-        image={data.album.images[0].url}
-        album={data.artists[0].name}
-      />
+    <div className="flex flex-col items-center p-5 bg-gray-800 space-y-5 overflow-auto">
+      <h3 className="text-white text-xl">Create Playlist</h3> 
+      {data.map((song) => {
+        const { id, name, artists, album } = song;
+        return (
+          <Song
+            key={id}
+            image={album.images[0]?.url}
+            title={name}
+            album={artists[0]?.name}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/components/Song/index.js
+++ src/components/Song/index.js
@@ -1,16 +1,12 @@
 const Song = ({ image, title, album }) => {
   return (
-    <div className="flex flex-row items-center rounded-md shadow-lg bg-white p-3">
-      <header className="">
-        <img
-          className="rounded-md h-32 w-32"
-          src={image}
-          alt="Album"
-        />
-      </header>
-      <div className="p-5">
-        <h5 className="text-gray-900 text-xl font-medium mb-2">{title}</h5>
-        <p className="text-gray-700 text-base mb-4">{album}</p>
+    <div className="flex flex-row justify-between items-center rounded-md shadow-lg bg-white p-3 w-1/3">
+      <div className="flex items-center">
+        <img className="rounded-md h-16 w-16" src={image} alt="Album" />
+        <div className="p-2">
+          <h5 className="text-gray-900 text-xl font-medium">{title}</h5>
+          <p className="text-gray-700 text-base">{album}</p>
+        </div>
       </div>
       <button className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg">
         Select
