a443470d5d30061f625b8d4b06b97832372ac028
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2022-04-13 15:58:04 +0700

    Move REDIRECT_URL to env

--- src/helper/spotify.js
+++ src/helper/spotify.js
@@ -1,5 +1,5 @@
 var client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-var redirect_uri = "http://localhost:3000";
+var redirect_uri = process.env.REACT_APP_REDIRECT_URL;
 
 var state = "KMG2FE5131ANDIKA";
 
